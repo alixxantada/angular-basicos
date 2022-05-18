d67d558eb410faf83bd1981dcd53cc70a08d760f
Alejandro <alixxantada@gmail.com>
2022-05-18 11:04:13 +0200

    primer commit

--- src/app/contador/contador.modulo.ts
+++ src/app/contador/contador.modulo.ts
@@ -12,6 +12,7 @@ import { ContadorComponent } from './contador/contador.component';
     ],
     imports: [
         // Aqui dentro por lo general van otros modulos
+        // Hola
     ]
 })
 export class ContadorModule{}
